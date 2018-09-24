bfb05c0927c5b9fb5965f45e594d6af92e737fec
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-25 01:50:43 +0800

    引入mint vue-source

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.min.css'
+import '../lib/mui/dist/css/mui.min.css'
+import Router from "vue-router";
+import VueResource from 'vue-resource';
+
+
+Vue.use(VueResource);
+
+
+Vue.use(Router)
+
+Vue.use(MintUI)
+
+
 
 Vue.config.productionTip = false
 
