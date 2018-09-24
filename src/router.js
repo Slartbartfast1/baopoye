8c04b2dd5fdc0f1ca47f52d96d5ca35d5924768c
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-25 01:50:27 +0800

    引入新闻

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import news from './components/news.vue'
+import newsContent from './components/newsContent.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -13,13 +14,17 @@ export default new Router({
       name: 'home',
       component: Home
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+      {
+      path: '/news',
+      name: 'news',
+      component: news
+    },
+      {
+      path: '/newsContent',
+      name: 'newsContent',
+      component: newsContent
+    },
+
+  ],
+    linkActiveClass:'mui-active'
 })
