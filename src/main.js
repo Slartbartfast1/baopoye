d8115e7fc8d2ccde25292c4644427e56a6b277c2
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-25 21:28:39 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 import router from './router'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.min.css'
 import '../lib/mui/dist/css/mui.min.css'
+import '../lib/mui/dist/css/icons-extra.css'
+import '../lib/mui/dist/fonts/mui-icons-extra.ttf'
 import Router from "vue-router";
 import VueResource from 'vue-resource';
+
+
 
 
 Vue.use(VueResource);
@@ -21,5 +26,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
