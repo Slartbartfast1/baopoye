62a7ab21e406461986ff5a27171a6527f3c559db
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-25 21:29:10 +0800

    新闻内容

--- src/components/newsContent.vue
+++ src/components/newsContent.vue
@@ -1,30 +1,28 @@
 <template>
     <div>
-        <mt-header title="爆破页">
+        <mt-header title="新闻" fixed="true">
             <router-link to="/news" slot="left">
                 <mt-button icon="back">返回</mt-button>
             </router-link>
-            <mt-button icon="more" slot="right"></mt-button>
         </mt-header>
         <iframe :src="this.$route.query.newsUrl" frameborder="0"></iframe>
     </div>
 </template>
 
 <script>
-
     export default {
-        mounted: function () {
-
-            console.log(this.$route.query.newsUrl)
-        },
         name: "newsContent",
     }
 </script>
 
-
-<style scoped>
-iframe{
-    width: 100%;
-    height:100vh;
-}
+<style scoped lang="scss">
+    div {
+        padding-bottom: 50px;
+        height: calc(100vh - 50px);
+        iframe {
+            display: block;
+            width: 100%;
+            height: calc(100vh - 50px);
+        }
+    }
 </style>
