da94e0f09e4af49323f4bb097ab7f4ed2562c449
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-25 21:27:52 +0800

    博客后台页

---
+++ src/components/admin.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="box" >
+        <mt-header title="博客后台" fixed="true">
+            <router-link to="/#" slot="left">
+                <mt-button icon="back">返回</mt-button>
+            </router-link>
+        </mt-header>
+        <iframe src="https://slartbartfast.cn/admin/login.php" frameborder="0"></iframe>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "admin"
+    }
+</script>
+
+<style scoped lang="scss">
+    #box {
+        padding-bottom:50px;
+        height: calc(100vh - 50px);
+        iframe {
+            display: block;
+            width: 100%;
+            height:calc(100vh - 50px);
+        }
+    }
+</style>
