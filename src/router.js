2696aa4217f7ceaa00d7b16d12110111dbe4b409
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-26 15:47:39 +0800

    修改模式

--- src/router.js
+++ src/router.js
@@ -9,7 +9,7 @@ import admin from './components/admin.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
